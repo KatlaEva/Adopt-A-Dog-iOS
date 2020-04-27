fde65bbda597440e37660a9ee458ce57d4e48c3d
KatlaEva <katla@shape.dk>
2020-04-27 11:12:43 +0200

    Initial Commit

---
+++ Adopt-A-DogTests/Adopt_A_DogTests.swift
@@ -0,0 +1,34 @@
+//
+//  Adopt_A_DogTests.swift
+//  Adopt-A-DogTests
+//
+//  Created by Þórkatla Eva Víkingsdóttir on 27/04/2020.
+//  Copyright © 2020 Þórkatla Eva Víkingsdóttir. All rights reserved.
+//
+
+import XCTest
+@testable import Adopt_A_Dog
+
+class Adopt_A_DogTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
